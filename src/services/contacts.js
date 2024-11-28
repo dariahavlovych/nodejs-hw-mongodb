6114738b5226a03e2a9d038eec0701f55ad45013
Daria Havlovych <daria.havlovych@gmail.com>
2024-11-28 22:15:46 +0200

    add pagination

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,23 @@
 import { contactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = () => {
-  return contactsCollection.find();
+export const getAllContacts = async ({ page, perPage }) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = contactsCollection.find();
+  const contactsCount = await contactsCollection
+    .find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = (contactId) => {
